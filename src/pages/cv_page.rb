a0370a5a77b0fdf5a4f420863810de69f41d40a7
Jai Sharma <jai@jai.moe>
2022-10-21 02:23:02 +0100

    Update CV page print CSS

--- src/pages/cv_page.rb
+++ src/pages/cv_page.rb
@@ -8,6 +8,8 @@ module Pages
 
     def template
       render Layout.new("cv") do
+        add_cv_css
+
         render Grid.new(
           tl: cv[:info][:name],
           tr: cv[:info][:email],
@@ -17,18 +19,7 @@ module Pages
 
         render Section.new("Experience") do
           cv[:experience].each do |work|
-            render Grid.new(
-              tl: work[:company],
-              tr: work[:location],
-              bl: work[:position],
-              br: "#{work[:start_date]} - #{work[:end_date]}",
-            )
-
-            ul do
-              work[:details].each do |point|
-                li point
-              end
-            end
+            render WorkSection.new(work)
           end
         end
 
@@ -40,31 +31,35 @@ module Pages
               bl: edu[:qualification],
               br: "#{edu[:start_date]} - #{edu[:end_date]}",
             )
-
-            ul do
-              edu[:details].each do |point|
-                li point
-              end
-            end
           end
         end
 
         render Section.new("Projects") do
           cv[:projects].each do |project|
-            h4 style: "margin-bottom: 0;" do
-              text "#{project[:name]} "
-              a(href: project[:link]) { "(link)" } if project[:link]
-            end
-            p style: "margin-top: 5px" do
-              project[:summary]
+            div class: "cv-info" do
+              h3 class: "cv-info-title" do
+                text "#{project[:name]} "
+                text "("
+                a(href: project[:link]) { project[:link].delete_prefix("https://") } if project[:link]
+                text ")"
+              end
+              p class: "cv-info-para" do
+                project[:summary]
+              end
             end
           end
         end
 
         render Section.new("Programming Skills") do
           cv[:skills].each do |skill|
-            h4(style: "margin-bottom: 0;") { skill[:key] }
-            p(style: "margin-top: 5px") { skill[:values].join(", ") }
+            div class: "cv-info" do
+              h3 class: "cv-info-title" do
+                skill[:key]
+              end
+              p class: "cv-info-para" do
+                skill[:values].join(", ")
+              end
+            end
           end
         end
       end
@@ -76,27 +71,105 @@ module Pages
       @cv ||= YAML.load_file('./cv.yml', symbolize_names: true)
     end
 
-    class Grid < Phlex::View
-      ROW_STYLE = <<~CSS
+    def add_cv_css
+      raw "<style>#{PAGE_CSS}</style>"
+      raw "<style>#{PRINT_CSS}</style>"
+    end
+
+    PAGE_CSS = <<~CSS
+      .cv-grid {
+        margin-bottom: var(--spacing);
+      }
+
+      .cv-grid-row {
         display: flex;
         flex-direction: row;
         align-items: center;
         justify-content: space-between;
-      CSS
+      }
 
+      .cv-grid-row > * {
+        margin: 0;
+      }
+
+      .cv-grid-row:last-child {
+        text-align: right;
+      }
+
+      .cv-section-header {
+        margin: var(--spacing) 0;
+      }
+
+      .cv-section-title {
+        text-transform: uppercase;
+        margin: 0;
+      }
+
+      .cv-section-divider {
+        width: var(--content-width);
+        border-top: 2px solid var(--color-divider);
+      }
+
+      .cv-work-details {
+        margin: var(--spacing) 0;
+      }
+
+      .cv-work-details-list {
+        padding-left: 2ch;
+        margin: 0;
+      }
+
+      .cv-info {
+        margin: var(--spacing) 0;
+      }
+
+      .cv-info-title {
+        margin: 0;
+      }
+
+      .cv-info-para {
+        margin: 0;
+      }
+    CSS
+
+    PRINT_CSS = <<~CSS
+      @page {
+        size: A4;
+        padding: 0;
+        margin: 5px;
+      }
+
+      @media print {
+        :root {
+          --content-width: 100%;
+          --content-text-size: 12px;
+          --spacing: 10px;
+        }
+
+        nav {
+          display: none;
+        }
+
+        .cv-info-title > a{
+          color: unset;
+        }
+      }
+    CSS
+
+    class Grid < Phlex::View
       def initialize(tl:, tr:, bl:, br:)
         @tl, @tr, @bl, @br = tl, tr, bl, br
       end
 
       def template
-        div style: "margin-bottom: 15px" do
-          div style: ROW_STYLE do
-            h3(style: "margin: 0") { @tl }
-            span(style: "text-align: right;") { @tr }
+        div class: "cv-grid" do
+          div class: "cv-grid-row" do
+            h3 @tl
+            span @tr
           end
-          div style: ROW_STYLE do
+          div class: "cv-grid-row" do
             span @bl
-            span(style: "text-align: right;") { @br }
+            span @br
           end
         end
       end
@@ -108,10 +181,33 @@ module Pages
       end
 
       def template(&b)
-        hr
-        h3(style: "text-transform: uppercase;") { @title }
-        hr
+        div class: "cv-section-header" do
+          h3(class: "cv-section-title") { @title }
+          div class: "cv-section-divider"
+        end
         yield_content(&b)
+      end
+    end
+
+    class WorkSection < Phlex::View
+      def initialize(work)
+        @work = work
+      end
+
+      def template
+        div class: "cv-work-details" do
+          render Grid.new(
+            tl: @work[:company],
+            tr: @work[:location],
+            bl: @work[:position],
+            br: "#{@work[:start_date]} - #{@work[:end_date]}",
+          )
+          ul class: "cv-work-details-list" do
+            @work[:details].each do |item|
+              li(class: "cv-work-details-list-item") { item }
+            end
+          end
+        end
       end
     end
   end
